718161f3bd86fc69b696e0d52646370330111fce
ks2355244 <ks2355244@yahoo.com>
2016-06-08 17:36:26 +0800

    copy others again

--- factorial.rb
+++ factorial.rb
@@ -1,9 +1,39 @@
 # Implement an iterative version of the factorial function
 def factorial_iterative(n)
-end
+		(1..n).inject(1, :*)
 
-# Implement a recursive version of the factorial function
-def factorial_recursive(n)
-end
-
-puts factorial_recursive(9) == 362880
+	# OR a more manual method
+	
+	=begin
+		result = 1
+		while n > 0
+			result *= n
+			n -= 1
+		end
+		result
+	=end
+	
+	end
+	
+	def fac(n)
+		counter = 0
+		until counter = n
+			num = n * (n-1)
+			n -= 1
+			counter -= 1
+		end
+	end
+	
+	
+	# Implement a recursive version of the factorial function
+	def factorial_recursive(n)
+		if n == 1
+			return 1
+		else
+			n * factorial_recursive(n-1)
+		end
+	end
+	
+	
+	puts factorial_iterative(9)	== 362880
+	puts factorial_recursive(9) == 362880
